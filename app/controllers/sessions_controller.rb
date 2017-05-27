2fe2077f261840567a1d0a5ef827b91b33f488cd
sj <grinpeace101@gmail.com>
2017-05-27 10:23:28 +0000

    edit login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,34 @@ class SessionsController < ApplicationController
   end
 
   def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email, password)
+      flash[:success] = 'ログインに成功したばい。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ログインに失敗したばい。'
+      render 'new'
+    end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = 'ログアウトしました〜'
+    flash[:success] = 'ログアウトしたばい。'
     redirect_to root_path
+  end
+
+  private
+
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      # ログイン成功
+      session[:user_id] = @user.id
+      return true
+    else
+      # ログイン失敗
+      return false
+    end
   end
 end
